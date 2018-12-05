3d097e85f297b95d4a4ccd8899f19a81dfa31a29
Robert Sheehan <robertnsheehan@gmail.com>
2018-12-05 14:41:37 +0000

    Initialisation of FFT calculation project

---
+++ Spectrum/Spectrum/Main.cpp
@@ -0,0 +1,19 @@
+#ifndef ATTACH_H
+#include "Attach.h"
+#endif // !ATTACH_H
+
+// The aim of this project is to implement the code needed to compute the FFT of an arbitrary data set
+// The data I have in mind is that obtained from a high speed oscilloscope
+// Generally, the data will be of the form (time_value, voltage_value)
+// The code for computing the FFT is taken from NRinC, this code adapts an existing code to run using std::vector<double>
+// R. Sheehan 5 - 12 - 2018
+
+int main() 
+{
+	std::cout << "Hello World\n"; 
+
+	std::cout << "Press enter to close\n"; 
+	std::cin.get(); 
+
+	return 0;
+}
