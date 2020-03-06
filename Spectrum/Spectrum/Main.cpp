03b7fd9e02de88b0a83f78b31dd2538f901094e8
Robert Sheehan <robertnsheehan@gmail.com>
2020-03-06 14:40:47 +0000

    Updated code for MSVS2019

--- Spectrum/Spectrum/Main.cpp
+++ Spectrum/Spectrum/Main.cpp
@@ -11,9 +11,9 @@
 
 int main() 
 {
-	//testing::sample_FFT_calculation(); 
+	testing::sample_FFT_calculation(); 
 
-	testing::laser_FFT();  
+	//testing::laser_FFT();  
 
 	std::cout << "Press enter to close\n"; 
 	std::cin.get(); 
